c976386772c5df0ddf4f2b399b56fea957c9ee21
aneerati <aneerati2004@gmail.com>
2025-07-22 18:42:11 -0400

    created basic user classes

--- loreorbit/src/main/java/com/celestial/loreorbit/LoreOrbitApplication.java
+++ loreorbit/src/main/java/com/celestial/loreorbit/LoreOrbitApplication.java
@@ -1,4 +1,4 @@
-package com.celestial.LoreoOrbit;
+package com.celestial.loreorbit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
