1435979a3774d89f5ed4ade114bd2b5ba0d3ca7f
aneerati <aneerati2004@gmail.com>
2025-08-01 17:06:18 -0400

    changed to maven. trying to sync server and frontend

--- loreorbit/src/main/java/com/celestial/loreorbit/LoreOrbitApplication.java
+++ loreorbit/src/main/java/com/celestial/loreorbit/LoreOrbitApplication.java
@@ -2,20 +2,12 @@ package com.celestial.loreorbit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
 @SpringBootApplication
-public class LoreOrbitApplication {
-
-	@RequestMapping("/")
-	String home() {
-		return "Something happened a long time ago";
-	}
+public class LoreorbitApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LoreOrbitApplication.class, args);
+		SpringApplication.run(LoreorbitApplication.class, args);
 	}
 
 }
